88353a325f251cf7c4b9a08efbc9069472ead2e2
Willstah <Willstah@Willstah-PC>
2013-10-18 21:00:51 -0400

    initial commit

---
+++ ConsoleApplication2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+namespace ConsoleSender
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte[] data = new byte[1024];
+            string input;
+            IPEndPoint ipep = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 1165);
+            Socket server = new Socket(AddressFamily.InterNetwork,SocketType.Dgram, ProtocolType.Udp);
+            EndPoint Remote = (EndPoint)ipep;
+            while (true)
+            {
+                input = "This is a test string*****************************************";
+                server.SendTo(Encoding.ASCII.GetBytes(input), Remote);
+                //Console.WriteLine(input);
+            }
+        }
+    }
+}
