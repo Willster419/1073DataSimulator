585a63d882aae04c7988594c857a09ca126c0de4
Willster419 <willster419@outlook.com>
2014-06-12 22:02:16 -0400

    uses the udpClient class instead of making my own

--- udpsenderconsole/Program.cs
+++ udpsenderconsole/Program.cs
@@ -11,32 +11,34 @@ namespace ConsoleSender
     {
         static void Main(string[] args)
         {
-            string robotSim1 = "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA1,13.58,1.11,0.22,0.33,0.24,1,1,1,1,1,1,1,1.11,1.11,1.11,1.11,1.11,1,1,2,1,250,1.11,110,1.11,1.11,500,5,69,1";
-            string robotSim2 = "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA1,13.25,1.11,-0.22,-0.33,-0.24,0,0,0,0,0,0,0,1.11,1.11,1.11,1.11,1.11,0,0,1,1,250,1.11,110,1.11,1.11,500,5,69,1";
+            string robotSim1 = "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA1,13.58,1.11,0.22,0.33,0.24,1,1,1,1,1,1,1,1.11,1.11,1.11,1.11,1.11,1,1,2,0,250,1.11,110,1.11,1.11,500,5,67,1";
+            string robotSim2 = "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA1,13.25,1.11,-0.22,-0.33,-0.24,0,0,0,0,0,0,0,1.11,1.11,1.11,1.11,1.11,0,0,1,1,250,1.11,110,1.11,1.11,500,5,67,1";
             IPEndPoint ipep = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 1165);
-            Socket server = new Socket(AddressFamily.InterNetwork,SocketType.Dgram, ProtocolType.Udp);
-            EndPoint Remote = (EndPoint)ipep;
+            UdpClient client = new UdpClient();
+            client.Client.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true);
+            client.Connect(ipep);
             string consoleSim;
             IPEndPoint console = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 6666);
-            Socket consoleServer = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
-            EndPoint RemoteConsole = (EndPoint)console;
+            UdpClient Cclient = new UdpClient();
+            Cclient.Client.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true);
+            Cclient.Connect(console);
             int count = 0;
             int sendingSim1 = 2;
             while (true)
             {
                 if (sendingSim1 % 2 == 0)
                 {
-                    server.SendTo(Encoding.ASCII.GetBytes(robotSim1), Remote);
+                    client.Send(Encoding.ASCII.GetBytes(robotSim1), robotSim1.Length);
                     System.Threading.Thread.Sleep(7);
                 }
                 else
                 {
-                    server.SendTo(Encoding.ASCII.GetBytes(robotSim2), Remote);
+                    client.Send(Encoding.ASCII.GetBytes(robotSim2), robotSim2.Length);
                     System.Threading.Thread.Sleep(7);
                 }
                 //Console.WriteLine(input);
                 consoleSim = "This is a test string "+count+++"\n";
-                consoleServer.SendTo(Encoding.ASCII.GetBytes(consoleSim), RemoteConsole);
+                Cclient.Send(Encoding.ASCII.GetBytes(consoleSim), consoleSim.Length);
                 //Console.WriteLine(input);
                 if (count == int.MaxValue) count = 0;
                 if (sendingSim1 == int.MaxValue) sendingSim1 = 2;
